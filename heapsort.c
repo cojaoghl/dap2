286d2850666f42d3b15962c9302886a6efe72ac3
aminco <aminco@me.com>
2023-05-02 07:39:27 +0200

    Update Tue May  2 07:39:21 AM CEST 2023

--- heapsort.c
+++ heapsort.c
@@ -80,36 +80,75 @@ void print_list(void) { /* this function just prints the integers stored in nums
 	for(i=0;i<numbers;i++) printf("%d\n",nums[i]);
 }
 
-long quicksort(int *a,int s,int t) { /*  sort a[s],...,a[t] */
-	int i=s,j=s,k=t,temp,pivot=a[s];
-	long comp=0;
+int *left_child(int *x) { /* return the index of the left child, or -1 if there is none */
+	int i=x-nums;
+	int j=2*(i+1)-1;
 
-	/* this is finally quicksort proper */
-	/* we use the Dijkstra partitioning method */
-	if(s>=t) return 0;
-	while(j<=k) {
-		comp++;
-		if(a[j]<pivot) {
-			temp=a[i]; a[i]=a[j]; a[j]=temp;
-			i++; j++;
-		}
-		else if(a[j]>pivot) {
-			temp=a[j]; a[j]=a[k]; a[k]=temp;
-			k--;
-		}
-		else j++;
+	return j<numbers ? nums+j : NULL;
+}
+
+int *right_child(int *x) { /* return the index of the right child, or -1 if there is none */
+	int i=x-nums;
+	int j=2*(i+1);
+
+	return j<numbers ? nums+j : NULL;
+}
+
+int *parent(int *x) { /* return the index of the parent, or -1 if there is none */
+	int i=x-nums;
+
+	return i>0 ? nums+(i+1)/2 : NULL;
+}
+
+void maxHeapify(int *i) { /* apply MaxHeapify from heap entry i */
+	int *left,*right,*max;
+
+	if(!i) return;	/* stop if i is not a valid heap entry */
+	left=left_child(i);	/* find left/right child of i */
+	right=right_child(i);
+
+	if(left && *left>*i) max=left; else max=i; /* compute max of i and its children */
+	if(right && *right>*max) max=right;
+
+	if(max!=i) {	/* if i is not the largest entry, swap i and the larger child ... */
+		int z=*i;	/* ...swap... */
+		*i=*max;
+		*max=z;
+		maxHeapify(max); /* ...and recurse */
 	}
-	return comp + quicksort(a,s,i-1) +quicksort(a,k+1,t);
+}
+
+void buildMaxHeap(void) {
+	int i;
+
+	for(i=numbers/2;i>=0;i--) maxHeapify(nums+i); /* run maxHeapify on top half of entries */
+}
+
+
+void heapsort(void) { /*  numbers */
+	int i;
+	int size=numbers;	/* memorise the original size of the array */
+
+	buildMaxHeap();	/* turn the array into a heap */
+	for(i=numbers-1;i>=1;i--) {	/* go over elements in reverse order */
+		int z=nums[0];	/* swap the head and tail elements */
+		nums[0]=nums[i];
+		nums[i]=z;
+
+		numbers--;	/* decrease current size */
+		maxHeapify(nums);	/* put the max of current heap in head position */
+	}
+	numbers=size;	/* restore the array size */
 }
 
 int main(int argc,char **argv) {
 	size=read_input();	/* attempt to read input */
 	if(size<=0) {fputs("Error: could not read input",stderr); return 1;} /* if reading the input fails, report error and abort */
 	numbers=convert_to_int();	/* attempt conversion to integers */
-	if(numbers<0) {fputs("Error: no numbers read",stderr); return 1;} /* if an error is reported back, abort */
+	if(numbers<=0) {fputs("Error: no numbers read",stderr); return 1;} /* if an error is reported back, abort */
 
-	printf("%ld\n",quicksort(nums,0,numbers-1)); /* perform quicksort on nums */
-/*	print_list();	 output the sorted list */
+	heapsort(); /* perform heapsort on nums */
+	print_list();	 /* output the sorted list */
 
 	return 0; /* report "success" */
 }
